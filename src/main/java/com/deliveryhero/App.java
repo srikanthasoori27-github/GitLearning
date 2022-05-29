610054ff853f14b17aa9802635fb1b43a9d823df
Srikanth <srikanth.asoori@sailpoint.com>
2022-05-29 20:11:29 +0530

    first project

---
+++ src/main/java/com/deliveryhero/App.java
@@ -0,0 +1,13 @@
+package com.deliveryhero;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
